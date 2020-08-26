44e610538676ac02bbc24873575b39b8afb1daac
Ryan Altimari <ryanaltimari8@gmail.com>
2020-08-26 11:33:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,5 @@
 
 
 def prime?(num)
-if (num == 0)
-	puts "0 is not prime"
-else
-	
-	i = 2
-	while(i < num)
-		if (num % i == 0)
-			count += 1
-		end
-		i += 1
-	end
-	end
-	if count > 1
-		puts "#{num} is not a prime number"
-	else
-		puts "#{num} is a prime number"
-	end
+
 end
